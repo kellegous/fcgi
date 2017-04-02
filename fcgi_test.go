190f95d8ac41c0e9f561ccfa89ff24b25ed8fdff
Kelly Norton <kellegous@gmail.com>
2017-04-02 17:51:53 -0400

    Added a concurrent request test for stdout.

--- fcgi_test.go
+++ fcgi_test.go
@@ -3,6 +3,7 @@ package fcgi
 import (
 	"bufio"
 	"bytes"
+	"crypto/rand"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -128,6 +129,38 @@ func paramsFor(verb string,
 	}
 
 	return p
+}
+
+func newRandomData(t *testing.T, n int) []byte {
+	b := make([]byte, n)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		t.Fatal(err)
+	}
+	return b
+}
+
+func concat(bs ...[]byte) []byte {
+	n := 0
+	for _, b := range bs {
+		n += len(b)
+	}
+	buf := make([]byte, n)
+
+	o := 0
+	for _, b := range bs {
+		copy(buf[o:], b)
+		o += len(b)
+	}
+
+	return buf
+}
+
+func TestConcat(t *testing.T) {
+	a := []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05}
+	b := concat([]byte{0x00}, []byte{0x01, 0x02}, []byte{0x03, 0x04, 0x05})
+	if !bytes.Equal(a, b) {
+		t.Fatalf("%v vs %v", a, b)
+	}
 }
 
 func TestStatusOK(t *testing.T) {
@@ -341,4 +374,104 @@ func TestHeaders(t *testing.T) {
 		},
 		[]byte{})
 
+}
+
+func TestConcurrencyOnStdout(t *testing.T) {
+	s := newServer(t)
+	defer s.Close()
+
+	m := map[string]chan []byte{
+		"A": make(chan []byte),
+		"B": make(chan []byte),
+	}
+
+	// this provides a sync barrier for each step in this test.
+	wall := make(chan struct{})
+
+	s.Serve(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		name := r.Header.Get("Name")
+		for b := range m[name] {
+			if _, err := w.Write(b); err != nil {
+				t.Fatal(err)
+			}
+			wall <- struct{}{}
+		}
+	}))
+
+	c, err := Dial(s.Network(), s.Addr())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	var aout, aerr bytes.Buffer
+	ra, err := c.BeginRequest(
+		paramsFor("GET", map[string][]string{
+			"HTTP_NAME": {"A"},
+		}),
+		nil, &aout, &aerr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var bout, berr bytes.Buffer
+	rb, err := c.BeginRequest(
+		paramsFor("GET", map[string][]string{
+			"HTTP_NAME": {"B"},
+		}),
+		nil, &bout, &berr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	aData := newRandomData(t, 2048)
+	bData := newRandomData(t, 2048)
+
+	go func() {
+		if err := ra.Wait(); err != nil {
+			t.Fatal(err)
+		}
+
+		mustHaveRequest(t,
+			&aout,
+			http.StatusOK,
+			nil,
+			aData)
+
+		wall <- struct{}{}
+	}()
+
+	go func() {
+		if err := rb.Wait(); err != nil {
+			t.Fatal(err)
+		}
+
+		mustHaveRequest(t,
+			&bout,
+			http.StatusOK,
+			nil,
+			bData)
+
+		wall <- struct{}{}
+	}()
+
+	// This ensures that the two requests send their stdout back in an
+	// interleaved fashion.
+	m["B"] <- bData[:1024]
+	<-wall
+
+	m["A"] <- aData[:1024]
+	<-wall
+
+	m["B"] <- bData[1024:]
+	<-wall
+
+	m["A"] <- aData[1024:]
+	<-wall
+
+	close(m["B"])
+	close(m["A"])
+
+	<-wall
+	<-wall
 }
